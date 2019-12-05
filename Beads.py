3b9c4840b7490e2124648df1939788ba80dd95c3
Simon Pincus <simon.pincus@gmail.com>
2019-12-05 13:12:29 +1000

    Moved step function to Beads.py

--- Beads.py
+++ Beads.py
@@ -12,6 +12,7 @@ def make_random_beads(count, max_link = 0):
             while (np.linalg.norm(links) > max_link):
                 links = np.random.normal(0,1,3)
         beads[i,:] = beads[i-1,:] + links
+    move_center_of_mass_to_origin(beads)
     return beads
 
 def move_center_of_mass_to_origin(beads):
@@ -34,4 +35,55 @@ def get_forces(links, b):
     norm = np.linalg.norm(links, axis=1)
     norm_sq = np.square(norm)
     return np.transpose(np.multiply(np.transpose(links),1/(1-norm_sq/b)))
+
+def step_FENE_semi_implicit(beads, b, tol, dt, shear):
+    """Step the simulation forward by a given time step"""
+    links_corr = calculate_links_correction(beads, b, tol, dt, shear)
+    move_beads_using_links(beads, links_corr)
+    move_center_of_mass_to_origin(beads)
+
+def calculate_links_correction(beads, b, tol, dt, shear):
+    """Calculate semi implicit predictor-corrector integration of a FENE spring"""
+    links = get_links(beads)
+    dW = np.random.normal(0,dt**0.5,(len(beads)-1,3))
+    forces = get_forces(links, b)
+    k = np.array([[0, shear, 0], [0, 0, 0], [0, 0, 0]])
+    flow_term = np.transpose(np.dot(k,np.transpose(links)))
+
+    # predicted change in beads
+    prev_forces = np.insert(forces[0:-1],0,0,axis=0)
+    next_forces = np.insert(forces[1:],len(forces)-1,0,axis=0)
+    links_pred = links + (0.25*(prev_forces - 2*forces + next_forces) + \
+                flow_term)*dt + 1/np.sqrt(2)*dW
+    links_corr = np.copy(links_pred)
+
+    # Corrector steps
+    epsilon = 20
+    while epsilon > tol:
+        flow_term_pred = np.transpose(np.dot(k,np.transpose(links_pred)))
+        for i in range(len(links)):
+            gamma = links[i,:] + (0.5*(flow_term[i,:] + flow_term_pred[i,:]) + \
+                    0.25*(prev_forces[i,:] + next_forces[i,:]))*dt + 1/np.sqrt(2)*dW[i,:]
+            norm_gamma = np.linalg.norm(gamma)
+            gamma_direction = gamma/norm_gamma
+            linksl = get_root(norm_gamma, b, dt)
+            links_corr[i,:] = gamma_direction*linksl
+            linksl2 = linksl**2
+            if i!=len(links)-1:
+                prev_forces[i+1,:] = links_corr[i,:]*(1/(1-linksl2/b))
+            if i!=0:
+                next_forces[i,:] = links_corr[i,:]*(1/(1-linksl2/b))
+        epsilon = np.sum((links_pred-links_corr)**2)
+        links_pred = np.copy(links_corr)    
+    return links_corr
+
+def get_root(norm_gamma, b, dt):
+    """Find the first root in range"""
+    coeff = [1, -norm_gamma, -b*(1+0.5*dt), b*norm_gamma]
+    roots = np.roots(coeff)
+    for root in roots:
+        if (root>0)&(root<np.sqrt(b)):
+            return root
+    raise Exception('no root found')
+    
 
--- Visualisation.py
+++ Visualisation.py
@@ -2,67 +2,15 @@
 
 import numpy as np
 import Constants as c
-from Beads import *
+import Beads as b
 import View as v
 
-def step_FENE_semi_implicit(beads, k, b, dt):
-    # Semi implicit predictor-corrector integration of a FENE spring
-    links = get_links(beads)
-    dW = np.random.normal(0,dt**0.5,(c.BEAD_COUNT-1,3))
-    Fc = get_forces(links, c.B)
-    flow_term = np.transpose(np.dot(k,np.transpose(links)))
-
-    # predicted change in beads
-    prev_forces = np.insert(Fc[0:-1],0,0,axis=0)
-    next_forces = np.insert(Fc[1:],len(Fc)-1,0,axis=0)
-    links_pred = links + (0.25*(prev_forces - 2*Fc + next_forces) + \
-                flow_term)*dt + 1/np.sqrt(2)*dW
-    links_corr = np.copy(links_pred)
-
-    # Corrector steps
-    epsilon = 20
-    while epsilon > c.TOL:
-        flow_term_pred = np.transpose(np.dot(k,np.transpose(links_pred)))
-        for i in range(len(links)):
-            gamma = links[i,:] + (0.5*(flow_term[i,:] + flow_term_pred[i,:]) + \
-                    0.25*(prev_forces[i,:] + next_forces[i,:]))*dt + 1/np.sqrt(2)*dW[i,:]
-            norm_gamma = np.linalg.norm(gamma)
-            gamma_direction = gamma/norm_gamma
-            coeff = [1, -norm_gamma, -b*(1+0.5*dt), b*norm_gamma]
-            roots = np.roots(coeff)
-            for root in roots:
-                if (root>0)&(root<np.sqrt(b)):
-                    linksl = root
-                    break
-            links_corr[i,:] = gamma_direction*linksl
-            linksl2 = linksl**2
-            if i!=len(links)-1:
-                prev_forces[i+1,:] = links_corr[i,:]*(1/(1-linksl2/b))
-            if i!=0:
-                next_forces[i,:] = links_corr[i,:]*(1/(1-linksl2/b))
-        epsilon = np.sum((links_pred-links_corr)**2)
-        links_pred = np.copy(links_corr)
-    move_beads_using_links(beads, links_corr)
-    move_center_of_mass_to_origin(beads)
-    return beads
-
-
-#flow tensor
-gamma_dot = v.get_shear_value()
-k = np.array([[0, gamma_dot, 0], [0, 0, 0], [0, 0, 0]])
-
-
 # set up bead vectors
-beads = make_random_beads(c.BEAD_COUNT, np.sqrt(c.B))
-move_center_of_mass_to_origin(beads)
-links = get_links(beads)
-v.create_elements(beads, links)
+beads = b.make_random_beads(c.BEAD_COUNT, np.sqrt(c.B))
+v.create_elements(beads, b.get_links(beads))
 
 while True:
     if v.is_running():
-        gamma_dot = v.get_shear_value()
-        k = np.array([[0, gamma_dot, 0], [0, 0, 0], [0, 0, 0]])
-        beads = step_FENE_semi_implicit(beads, k, c.B, v.get_speed_value())
-        links = get_links(beads)
-        v.update_elements(beads, links)
+        b.step_FENE_semi_implicit(beads, c.B, c.TOL, v.get_speed_value(), v.get_shear_value())
+        v.update_elements(beads, b.get_links(beads))
 
